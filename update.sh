8a5d8e01b51adc7d2dc92dbdf88ca1a337c3c767
David Hunter <davie@synackrst.net>
2011-08-08 16:20:58 -0700

    Fixed update.sh to only operate on home directory.

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ln -sf .homedir/bash_profile .bash_profile
-ln -sf .homedir/bashrc .bashrc
-ln -sf .homedir/gitconfig .gitconfig
-ln -sf .homedir/makepkg.conf .makepkg.conf
-ln -sf .homedir/screenrc .screenrc
-ln -sf .homedir/vimrc .vimrc
+ln -sf .homedir/bash_profile ~/.bash_profile
+ln -sf .homedir/bashrc ~/.bashrc
+ln -sf .homedir/gitconfig ~/.gitconfig
+ln -sf .homedir/makepkg.conf ~/.makepkg.conf
+ln -sf .homedir/screenrc ~/.screenrc
+ln -sf .homedir/vimrc ~/.vimrc
